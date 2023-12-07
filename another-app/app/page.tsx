0fcf0dbf211e940ffb482c7e57cc0b50be875c7f
Asif Hafeez <asif.hafeez@collinsongroup.com>
2023-12-07 10:48:22 +0000

    trigger a deploy

--- another-app/app/page.tsx
+++ another-app/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Deploying in the office. Environment deployed is: {environment}&nbsp;
+          Deploy in demo: {environment}&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
